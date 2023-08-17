97a2922307b115c5bbca7a0a77a9250b2e37d7a4
Student in flipflops <adam151710@gmail.com>
2023-08-17 13:51:31 +0200

    Initial commit, seven segment dispaly is working.

---
+++ python/mpts.py
@@ -0,0 +1,111 @@
+import tkinter as tk
+import time
+
+segment_patterns = [
+    "abcdef",  # 0
+    "bc",      # 1
+    "abdeg",   # 2
+    "abcdg",   # 3
+    "bcfg",    # 4
+    "acdfg",   # 5
+    "acdefg",  # 6
+    "abc",     # 7
+    "abcdefg", # 8
+    "abcfg"    # 9
+]
+
+segment_coords = {
+    "a": (10, 10, 60, 10),
+    "b": (62, 12, 62, 60),
+    "c": (62, 65, 62, 120),
+    "d": (10, 122, 60, 122),
+    "e": (7, 65, 7, 120),
+    "f": (7, 13, 7, 60),
+    "g": (10, 62, 60, 62)
+}
+
+def ring():
+    print("ring")
+
+def count_down(minutes, seconds, canvas, hours , break_time):
+    
+    
+    
+    if seconds == 0:
+        seconds = 59
+        minutes -= 1
+    else:
+        seconds -= 1
+
+    if minutes < 0 and break_time == False:
+        ring()
+        print("break time")
+        hours+=1
+        minutes = 0
+        seconds = 5
+        break_time = True
+    elif minutes < 0 and break_time == True:
+        ring()
+        print("study time")
+        minutes = 0
+        seconds = 10
+        break_time = False
+
+        
+
+    for x in "abcdefg":
+        if x in segment_patterns[minutes // 10]:
+            canvas.itemconfig("segment_10m_" + x, fill="red")
+        else:
+            canvas.itemconfig("segment_10m_" + x, fill="gray")
+    
+    for x in "abcdefg":
+        if x in segment_patterns[minutes % 10]:
+            canvas.itemconfig("segment_1m_" + x, fill="red")
+        else:
+            canvas.itemconfig("segment_1m_" + x, fill="gray")            
+    
+    for x in "abcdefg":
+        if x in segment_patterns[seconds // 10]:
+            canvas.itemconfig("segment_10s_" + x, fill="red")
+        else:
+            canvas.itemconfig("segment_10s_" + x, fill="gray")                
+    
+    for x in "abcdefg":
+        if x in segment_patterns[seconds % 10]:
+            canvas.itemconfig("segment_1s_" + x, fill="red")
+        else:
+            canvas.itemconfig("segment_1s_" + x, fill="gray")                    
+    
+    canvas.after(1000, count_down, minutes, seconds, canvas , hours , break_time)
+
+    
+
+def create_window():
+
+    window = tk.Tk()
+    window.title("Seven-Segment Countdown Clock")
+    
+    canvas = tk.Canvas(window, width=320, height=150)
+    canvas.pack()
+
+    for seg , coords in segment_coords.items():
+        canvas.create_line( coords[0]     , coords[1] ,coords[2]     , coords[3] , fill="gray", width=10, tags="segment_10m_"+seg  )
+        canvas.create_line( coords[0] +70 , coords[1] ,coords[2] +70 , coords[3] , fill="gray", width=10, tags="segment_1m_" +seg  )
+        canvas.create_line( coords[0] +170, coords[1] ,coords[2] +170, coords[3] , fill="gray", width=10, tags="segment_10s_"+seg  )
+        canvas.create_line( coords[0] +240, coords[1] ,coords[2] +240, coords[3] , fill="gray", width=10, tags="segment_1s_" +seg  )
+
+    canvas.create_rectangle(150 ,30 , 160 , 40 , fill="red"  )
+    canvas.create_rectangle(150 ,80 , 160 , 90 , fill="red"  )
+
+
+    countdown_minutes = 0
+    countdown_seconds = 5
+    working_hours = 0
+    break_time = False
+    count_down(countdown_minutes, countdown_seconds, canvas, working_hours , break_time)
+
+    window.mainloop()    
+
+
+create_window()
